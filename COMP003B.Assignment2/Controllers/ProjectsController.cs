8e1419aee8d397cfa791f7bdf51dc4eeebbfd729
briannaordaz <briannaordaz5@gmail.com>
2025-03-26 23:51:29 -0700

    created a partial view (_Navigation.cshtml)

--- COMP003B.Assignment2/Controllers/ProjectsController.cs
+++ COMP003B.Assignment2/Controllers/ProjectsController.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace COMP003B.Assignment2.Controllers;
 
-public class ProjectController : Controller
+public class ProjectsController : Controller
 {
     
     public IActionResult Current()
